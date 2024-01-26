0b017d499d5a59373bb0751937ebb682443e93de
Dawson Fields <dawson@gofields.com>
2024-01-26 00:39:23 -0600

    update infra

--- scripts/update-infra.sh
+++ scripts/update-infra.sh
@@ -5,9 +5,9 @@ docker rmi dfields2016/bible-reference-explorer-infra:master
 curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/docker-compose.yml > docker-compose.yml
 
 curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/scripts/init-letsencrypt.sh > init-letsencrypt.sh
-curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/update-api.sh > update-api.sh
-curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/update-ui.sh > update-ui.sh
-curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/update.sh > update.sh
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/scripts/update-api.sh > update-api.sh
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/scripts/update-ui.sh > update-ui.sh
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/scripts/update.sh > update.sh
 
 
 docker-compose up -d
