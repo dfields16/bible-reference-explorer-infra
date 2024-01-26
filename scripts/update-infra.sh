cb6782ea9cd80a36a51307f4c2dc44a99eaaad21
Dawson Fields <dawson@gofields.com>
2024-01-25 22:32:28 -0600

    Update scripts

--- scripts/update-infra.sh
+++ scripts/update-infra.sh
@@ -2,6 +2,10 @@ docker-compose down
 
 docker rmi dfields2016/bible-reference-explorer-infra:master
 
-curl https://raw.githubusercontent.com/dfields16/dfields16/master/neo4j.yaml > docker-compose.yml
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/docker-compose.yml > docker-compose.yml
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/update-api.sh > update-api.sh
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/update-ui.sh > update-ui.sh
+curl https://raw.githubusercontent.com/dfields16/bible-reference-explorer-infra/master/update.sh > update.sh
+
 
 docker-compose up -d
