55dedbede6b83c972d1c099d49e31f6ddc24dfe8
Dawson Fields <dawson@gofields.com>
2024-01-27 11:51:53 -0600

    fix docker compose

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(bible-reference-explorer.com www.bible-reference-explorer.com)
+domains=(bible-reference-explorer.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dawson@gofields.com" # Adding a valid address is strongly recommended
